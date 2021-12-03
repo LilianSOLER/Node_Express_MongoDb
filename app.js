9d286fe4fdc9d3ba78d686f0a2b8268a7fa52bd1
SOLER Lilian <soler.lilian64@gmail.com>
2021-12-03 16:28:15 +0100

    Application Express de Base : usage de next, app.use, res.status

--- app.js
+++ app.js
@@ -2,4 +2,25 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('Request received');
+  next();
+  });
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({message: 'Hello World!'});
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Response sent');
+  next();
+  });
+
+
 module.exports = app;
