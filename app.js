c2a77acd49abd6dd4346abb51a15ea7277991032
SOLER Lilian <soler.lilian64@gmail.com>
2021-12-03 16:51:04 +0100

    Création d'une route get /api/stuff

--- app.js
+++ app.js
@@ -3,24 +3,41 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('Request received');
-  next();
-  });
-
-app.use((req, res, next) => {
-  res.status(201);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
-app.use((req, res, next) => {
-  res.json({message: 'Hello World!'});
-  next();
+app.use('/api/stuff', (req, res, next) => {
+  const stuff = [
+    {
+      _id: '1',
+      title: 'First',
+      description: 'First description',
+      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      price: 1000,
+      userId: '1',
+    },
+    {
+      _id: '2',
+      title: 'Second',
+      description: 'Second description',
+      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      price: 2000,
+      userId: '2',
+    },
+    {
+      _id: '3',
+      title: 'Third',
+      description: 'Third description',
+      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      price: 3000,
+      userId: '3',
+    },
+  ];
+  res.status(200).json(stuff);
 });
-
-app.use((req, res, next) => {
-  console.log('Response sent');
-  next();
-  });
 
 
 module.exports = app;
