7d9eb3845f0f23661a017fcec6ee18367e5564a4
SOLER Lilian <soler.lilian64@gmail.com>
2021-12-03 16:58:38 +0100

    Création d'une route post /api/stuff - Utilisation de app.post et app.get au lieu de app.use et ajout de app.use(express.json()) afin de parser les données envoyer par l'utilisateur

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -9,7 +11,14 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api/stuff', (req, res, next) => {
+app.post('/api/stuff', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Object created!',
+  });
+});
+
+app.get('/api/stuff', (req, res, next) => {
   const stuff = [
     {
       _id: '1',
