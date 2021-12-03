f54ef1fe2c5682e9de6cb733a0773ea75624548f
SOLER Lilian <soler.lilian64@gmail.com>
2021-12-03 18:04:49 +0100

    Ajout connexion à la base de données MongoDb distante

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const urlDb = require('./configdb');
 
 const app = express();
+
+mongoose
+  .connect(urlDb, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to database'))
+  .catch(() => console.log('Connection failed'));
 
 app.use(express.json());
 
