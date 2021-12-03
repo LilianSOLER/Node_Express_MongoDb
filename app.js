e7b689753ae223d74e6ff1eb50680c49b1bea2bc
SOLER Lilian <soler.lilian64@gmail.com>
2021-12-03 18:23:25 +0100

    Modification de la route Get afin de retourner la liste des articles enregistrés dans la bdd

--- app.js
+++ app.js
@@ -32,33 +32,9 @@ app.post('/api/stuff', (req, res, next) => {
 });
 
 app.get('/api/stuff', (req, res, next) => {
-  const stuff = [
-    {
-      _id: '1',
-      title: 'First',
-      description: 'First description',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 1000,
-      userId: '1',
-    },
-    {
-      _id: '2',
-      title: 'Second',
-      description: 'Second description',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 2000,
-      userId: '2',
-    },
-    {
-      _id: '3',
-      title: 'Third',
-      description: 'Third description',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 3000,
-      userId: '3',
-    },
-  ];
-  res.status(200).json(stuff);
+  Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
 });
 
 
