30e616efe52fc6f9657f40fedcbb3f9367a6c34f
SOLER Lilian <soler.lilian64@gmail.com>
2021-12-04 20:37:35 +0100

    Modification du controller stuff pour effacer l'images d'un produit lors de ssa suppression

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -1,4 +1,5 @@
 const Thing = require('../models/Thing');
+const fs = require('fs');
 
 exports.createThing = (req, res, next) => {
   const thingObject = JSON.parse(req.body.thing);
@@ -37,15 +38,13 @@ exports.modifyThing = (req, res, next) => {
 
 exports.deleteThing = (req, res, next) => {
   Thing.findOne({_id: req.params.id})
-    .then(
-      (thing) => {
-        if(!thing) {
-          return res.status(404).json({error: new Error('Object not found !')});
-        }
-        if(thing.userId !== req.auth.userId){
-          return res.status(401).json({error: new Error('Unauthorized request !')});
-        }
-        Thing.deleteOne({_id: req.params.id}).then(() => {res.status(200).json({ message: 'Object deleted' })});
+    .then(thing => {
+      const filename = thing.imageUrl.split('/images/')[1];
+      fs.unlink(`images/${filename}`, () => {
+        Thing.deleteOne({_id: req.params.id})
+          .then(() => {res.status(200).json({ message: 'Object deleted' })})
+          .catch(error => res.status(400).json({ error }));
+      })          
     })
-    .catch((error) => {res.status(400).json({error: error })})
-    }
+    .catch(error => res.status(500).json({ error }));
+    };
