29b561ccde8e55b1bd129298e45ff81c0661c657
SOLER Lilian <soler.lilian64@gmail.com>
2021-12-04 20:16:47 +0100

    Ajout de multer dans la route Post et ajustement de son controller et modification de l'app afin de rendre le dossier image statique

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
@@ -24,6 +25,7 @@ app.use((req, res, next) => {
 });
 
 app
+  .use('/images', express.static(path.join(__dirname, 'images')))
   .use('/api/stuff', stuffRoutes)
   .use('/api/auth', userRoutes);
 
--- routes/stuff.js
+++ routes/stuff.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 const stuffCtrl = require('../controllers/stuff');
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
 router
-  .post('/', auth, stuffCtrl.createThing)
+  .post('/', auth, multer, stuffCtrl.createThing)
   .get('/:id', auth, stuffCtrl.getThing)
   .get('/', auth, stuffCtrl.getAllThings)
   .put('/:id', auth,  stuffCtrl.modifyThing)
